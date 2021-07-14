5b3ece7f220e311fc895a57dff4abc17a7b8db9d
Deep Singh <deep.singh@zensar.com>
2021-07-14 13:26:46 +0000

    Updated ZTDeployPackage.ps1

--- D365DataMigrationOptions/PowerShell/v1/ZTDeployPackage.ps1
+++ D365DataMigrationOptions/PowerShell/v1/ZTDeployPackage.ps1
@@ -7,8 +7,8 @@ param(
 [string]$PackageName = "ZTPackageDeployment" ,
 [string]$PackageDirectory = "D:\a\1\s\ZTPackageDeployment",
 [string]$LogsDirectory = "D:\a\1\s\D365DataMigrationOptions\PowerShell\v1\logs",
-[string]$PackageDeploymentPath ="D:\a\1\s\D365DataMigrationOptions\HSL.NFUM.PackageDeployer.dll" ,
-[string]$ToolingConnectorModulePath ="D:\a\1\s\D365DataMigrationOptions\PowerShell\v1\tcpm2",
+[string]$PackageDeploymentPath ="D:\a\1\s\D365DataMigrationOptions\PowerShell\v1\tcpm2",
+[string]$ToolingConnectorModulePath ="D:\a\1\s\D365DataMigrationOptions\PowerShell\v1\tcpm",
 [string]$Timeout = '00:30:00', #optional timeout for Import-CrmPackage, default to 1 hour and 20 min. See https://technet.microsoft.com/en-us/library/dn756301.aspx
 [int]$CrmConnectionTimeout = 2, 
 [string]$RuntimePackageSettings,
